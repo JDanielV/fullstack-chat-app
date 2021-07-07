c2d43792ce55f18560a1f22eefda2e2b9d0aaba6
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2021-07-06 21:47:19 -0700

    first commit - created & connected db

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL || "messenger", "postgres", "556216", {
+  dialect: "postgres",
   logging: false
 });
 
