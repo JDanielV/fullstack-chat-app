25684e21a8c1efa06d51d51d42de2f5209723f24
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2021-07-07 15:47:31 -0700

    just cleaned up a console.log

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -101,7 +101,6 @@ export const postMessage = (body) => async (dispatch) => {
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
     } else {
-
       dispatch(setNewMessage(data.message));
     }
 
