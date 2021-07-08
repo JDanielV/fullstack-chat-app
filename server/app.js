0fcda8dfc62ded7a6126c4876900a4456aaa9edb
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2021-07-07 23:56:07 -0700

    added check to post messages endpoint to verify the senderId is actually one of the two users in the conversation

--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ app.use(urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, "public")));
 
 app.use(function (req, res, next) {
+
   const token = req.headers["x-access-token"];
   if (token) {
     jwt.verify(token, process.env.SESSION_SECRET, (err, decoded) => {
--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -4,6 +4,7 @@ const onlineUsers = require("../../onlineUsers");
 
 // expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
 router.post("/", async (req, res, next) => {
+
   try {
     if (!req.user) {
       return res.sendStatus(401);
@@ -13,8 +14,22 @@ router.post("/", async (req, res, next) => {
 
     // if we already know conversation id, we can save time and just add it to message and return
     if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId });
-      return res.json({ message, sender });
+
+      // however, we do need to check if the sender of the message (senderId) is actually
+      // one of the users in the conversation (user1Id or user2Id)
+      let conversation = await Conversation.findConversation(
+        senderId,
+        recipientId
+      );
+
+      if (senderId === conversation.user1Id || senderId === conversation.user2Id) {
+        const message = await Message.create({ senderId, text, conversationId });
+        return res.json({ message, sender });
+      }
+      else {
+        return res.sendStatus(401);
+      }
+
     }
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
     let conversation = await Conversation.findConversation(
